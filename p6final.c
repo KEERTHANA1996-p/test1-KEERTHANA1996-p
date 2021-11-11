956efb5a1df41373d78fb69e5ccc48cb83cbf074
Keerthana.p <92919578+KEERTHANA1996-p@users.noreply.github.com>
2021-11-11 11:10:06 +0530

    Update p6final.c

--- p6final.c
+++ p6final.c
@@ -1,19 +1,20 @@
 #include<stdio.h>   
-void input(int *n, int a[*n])
+void input(int n, int a[n])
 {
     printf("Maximum odd numbers: ");                                                      
-    scanf("%d", &*n);                                                          
-    printf("Enter any %d Numbers: ", *n);                               
+    scanf("%d", &n);                                                          
+    printf("Enter any %d Numbers: ", n);                               
     for(int i = 0;i < *n;i++)                                                     
     scanf("%d",&a[i]);
+    return;
 }
  
  
-float odd_avg(int *n, int a[*n])
+float odd_avg(int n, int a[n])
 {
     int sum = 0,i,count=0;
     float avg;
-    for(i = 0;i < *n;i++) 
+    for(i = 0;i < n;i++) 
     {   
         if(a[i]%2 != 0 )
         {
@@ -29,10 +30,10 @@ float odd_avg(int *n, int a[*n])
     return avg;
     
 }
-void output(float avg, int *n, int a[*n])     /* print all the elements of the array and then the composite average */
+void output(float avg, int n, int a[n])     /* print all the elements of the array and then the composite average */
 {
     printf("\naverage of given odd numbers:\t");
-    for(int i = 0;i < *n;i++) 
+    for(int i = 0;i < n;i++) 
     {   
         if((a[i]%2) != 0 )
             printf("%d\t", a[i]);
@@ -42,7 +43,7 @@ void output(float avg, int *n, int a[*n])     /* print all the elements of the a
  
 void main()                                                                     
 {                                                                               
-    int *n,a[100];  
+    int n,a[100];  
     float avg;
     input(&n, a);
     avg = odd_avg(&n, a);
